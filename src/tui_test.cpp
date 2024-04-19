793f4660528ac59459769c6b4db6de0b0a14491e
Eduardo-Lomeli <a23110132@live.ceti.mx>
2024-04-18 19:59:33 -0600

    Version 2

--- src/tui_test.cpp
+++ src/tui_test.cpp
@@ -2,23 +2,45 @@
 #include <ftxui/screen/screen.hpp>
 #include <iostream>
 #include <string>
+#include <thread>  //cintrolar ciclos
+#include <ftxui/screen/color.hpp>
+#include <experimental/random>
 
 using namespace std;
 using namespace ftxui;
 
+
 int main(int argc, char const *argv[])
 {
-    const std::string texto = "Hola mundo";
-    Element textElement = text(texto);
-    Element dibujo = hbox(textElement | border);
+    int fotograma = 0;
+    string reset;
+    while (true)
+    {
+        fotograma ++;
 
-    Dimensions Alto = Dimension::Fixed(10);
-    Dimensions Ancho = Dimension::Fixed(10);
+        int r = experimental::randint(0,255);
+        int g = experimental::randint(0,255);
+        int b = experimental::randint(0,255);
 
-    Screen pantalla = Screen::Create(Ancho, Alto);
 
-    Render(pantalla, dibujo);
-    pantalla.Print();
-    cout << endl;
+        Element personaje = spinner(21, fotograma);
+        Decorator ColorFondo = bgcolor(Color::RGB(b,r,g));
+        Decorator ColorTexto = color(Color::RGB(r,g,b));
+        Element dibujo  = border({
+            hbox(personaje) | ColorFondo | ColorTexto
+        });
+
+        Dimensions Alto = Dimension::Fixed(10);
+        Dimensions Ancho = Dimension::Full();
+
+        Screen pantalla = Screen::Create(Ancho, Alto);
+
+        Render(pantalla, dibujo);
+        pantalla.Print();
+        reset = pantalla.ResetPosition();
+        cout << reset;
+        this_thread::sleep_for(0.5s);
+    }
+
     return 0;
 }
