3af8782f17cc9ac1891161270d9c0f9b2b724cb3
Eduardo-Lomeli <a23110132@live.ceti.mx>
2024-04-24 18:59:53 -0600

    Version 3

--- src/tui_test.cpp
+++ src/tui_test.cpp
@@ -5,6 +5,8 @@
 #include <thread>  //cintrolar ciclos
 #include <ftxui/screen/color.hpp>
 #include <experimental/random>
+#include <list>
+#include <fstream>
 
 using namespace std;
 using namespace ftxui;
@@ -12,8 +14,24 @@ using namespace ftxui;
 
 int main(int argc, char const *argv[])
 {
+    list<string> textos;
+    fstream imagen;
+    imagen.open("./assets/imagen.txt");
+    string linea;
+
+    while (getline(imagen, linea))
+    {
+        textos.push_back(linea);
+    }
+    imagen.close();
+    
+
+
+
     int fotograma = 0;
     string reset;
+    int posX = 0;
+    int posY = 0;
     while (true)
     {
         fotograma ++;
@@ -30,16 +48,30 @@ int main(int argc, char const *argv[])
             hbox(personaje) | ColorFondo | ColorTexto
         });
 
-        Dimensions Alto = Dimension::Fixed(10);
+        Dimensions Alto = Dimension::Full();
         Dimensions Ancho = Dimension::Full();
 
         Screen pantalla = Screen::Create(Ancho, Alto);
-
         Render(pantalla, dibujo);
+        
+        int l = 0;
+        for (auto &&texto : textos)
+        {
+            int i = 0;
+            for (auto &&letra : texto)
+            {
+                pantalla.PixelAt(posX +i, posY +l).character =letra;
+                i++;
+            }
+            l++;
+        }
+        posX++;
+        
+
         pantalla.Print();
         reset = pantalla.ResetPosition();
         cout << reset;
-        this_thread::sleep_for(0.5s);
+        this_thread::sleep_for(0.1s);
     }
 
     return 0;
